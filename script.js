1da2df3651fd5290571b5f2b641d5fd11635a205
Andrew Cooper <andrew.cooper@bbconsult.co.uk>
2019-05-16 12:47:53 +0700

    Added Clear button Expanded the list of characters to replace by separator

--- script.js
+++ script.js
@@ -1,51 +1,78 @@
 function showError(container, errorMessage) {
-    container.classList.add('error');
-    var msgElem = document.createElement('span');
-    msgElem.classList.add("error-message");
-    msgElem.innerHTML = errorMessage;
-    container.appendChild(msgElem);
+  container.classList.add("error");
+  var msgElem = document.createElement("span");
+  msgElem.classList.add("error-message");
+  msgElem.innerHTML = errorMessage;
+  container.appendChild(msgElem);
 }
 
 function resetError(container) {
-    if (container.classList.contains('error')) {
-        container.removeChild(container.lastChild)
-        container.classList.remove('error');
-    }
+  if (container.classList.contains("error")) {
+    container.removeChild(container.lastChild);
+    container.classList.remove("error");
+  }
 }
 
 function convertName(form) {
-    var elems = form.elements;
-    var result = document.getElementById("result");
-    var isError = false;
+  var elems = form.elements;
+  var result = document.getElementById("result");
+  var isError = false;
 
-    resetError(elems.tasknumber.parentNode)
-    if (!elems.tasknumber.value) {
-        showError(elems.tasknumber.parentNode, 'Set the task number');
-        isError = true;
+  resetError(elems.tasknumber.parentNode);
+  if (!elems.tasknumber.value) {
+    showError(elems.tasknumber.parentNode, "Set the task number");
+    isError = true;
+  }
+
+  resetError(elems.taskname.parentNode);
+  if (!elems.taskname.value) {
+    showError(elems.taskname.parentNode, "Set the task name");
+    isError = true;
+  }
+
+  if (!isError) {
+    var taskType = elems.tasktype.value;
+    var taskTypeName = taskType !== "" ? `${taskType}-` : "";
+    var taskNumber = elems.tasknumber.value.trim();
+    var taskName = elems.taskname.value
+      .trim()
+      .replace(/\./g, "-")
+      .replace(/\,/g, "-")
+      .replace(/\(/g, "-")
+      .replace(/\)/g, "-")
+      .replace(/\=/g, "-")
+      .replace(/\+/g, "-")
+      .replace(/\</g, "-")
+      .replace(/\>/g, "-")
+      .replace(/\[/g, "-")
+      .replace(/\]/g, "-")
+      .replace(/\s*-/g, "-")
+      .replace(/-\s*/g, "-")
+      .replace(/\s+/g, "-")
+      .replace(/\-\-/g, "-")
+      .toLowerCase();
+
+    if (taskName[taskName.length - 1] === "-") {
+      taskName = taskName.slice(0, taskName.length - 1);
     }
 
-    resetError(elems.taskname.parentNode)
-    if (!elems.taskname.value) {
-        showError(elems.taskname.parentNode, 'Set the task name');
-        isError = true;
-    }
-
-    if (!isError) {
-        var taskType = elems.tasktype.value;
-        var taskNumber = elems.tasknumber.value.trim();
-        var taskName = elems.taskname.value
-            .trim()
-            .replace(/\s*-/g, '-')
-            .replace(/-\s*/g, '-')
-            .replace(/\s+/g, '-')
-            .toLowerCase();
-        result.value = `${taskType}-${taskNumber}-${taskName}`;
-    }
+    result.value = `${taskTypeName}${taskNumber}-${taskName}`.replace(
+      /\-\-/g,
+      "-"
+    );
+  }
 }
 
 function copyToClipboard() {
-    var copyText = document.getElementById("result");
-    copyText.select();
-    document.execCommand("copy");
+  var copyText = document.getElementById("result");
+  copyText.select();
+  document.execCommand("copy");
 }
 
+function clearAll() {
+  var taskNumber = document.getElementById("tasknumber");
+  var taskName = document.getElementById("taskname");
+  var result = document.getElementById("result");
+
+  taskNumber.value = taskName.value = result.value = "";
+}
